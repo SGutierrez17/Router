a6380c21008ad80afd13aded25a23ed08bad9324
Sergio Gutiérrez <sergiogutierrez17080@gmail.com>
2024-03-31 13:27:39 -0500

    feat(exercise1): Added exercise1

---
+++ src/about.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default function About() {
+    return (
+        <div>
+            <h2>Acerca de</h2>
+            <p>Esta es la página "Acerca de".</p>
+        </div>
+    )
+}
---
+++ src/home.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default function Home() {
+    return (
+        <div>
+            <h2>Inicio</h2>
+            <p>Bienvenido a la página de inicio.</p>
+        </div>
+    )
+}
