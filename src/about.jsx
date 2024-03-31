4ea44ea646f6e8e9cb0d5ea8f8dd6ab71fee75a7
Sergio Gutiérrez <sergiogutierrez17080@gmail.com>
2024-03-31 13:36:55 -0500

    feat(exercise2): Added exercise2

--- src/about.jsx
+++ src/about.jsx
@@ -1,10 +1,18 @@
-import React from 'react'
+import React from 'react';
+
+import { Link } from 'react-router-dom';
 
 export default function About() {
     return (
         <div>
             <h2>Acerca de</h2>
-            <p>Esta es la página "Acerca de".</p>
+            <p>Page of About</p>
+            <nav>
+                <ul>
+                    <li><Link to="/">Inicio</Link></li>
+                    <li><Link to="/contact">Contacto</Link></li>
+                </ul>
+            </nav>
         </div>
     )
 }
--- src/home.jsx
+++ src/home.jsx
@@ -1,10 +1,18 @@
-import React from 'react'
+import React from 'react';
+
+import { Link } from 'react-router-dom';
 
 export default function Home() {
     return (
         <div>
-            <h2>Inicio</h2>
-            <p>Bienvenido a la página de inicio.</p>
+            <h1>Inicio</h1>
+            <p>Page of Home</p>
+            <nav>
+                <ul>
+                    <li><Link to="/about">Acerca de</Link></li>
+                    <li><Link to="/contact">Contacto</Link></li>
+                </ul>
+            </nav>
         </div>
     )
 }
